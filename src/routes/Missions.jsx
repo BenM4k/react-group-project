a680560a7ce22d25e6d49ba0d29f45d760f5e74b
vikas924 <obroy902@gmail.com>
2023-07-25 17:26:04 +0530

    Add join and leave funtionality and add styles

--- src/routes/Missions.jsx
+++ src/routes/Missions.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchmission } from '../redux/missions/missionSlice';
+import { fetchmission, joinmission, leavemission } from '../redux/missions/missionSlice';
 
 const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions.selectedmissions);
 
+  const handleclick = (id, reserved) => {
+    if (reserved === false) {
+      dispatch(joinmission(id));
+    } else {
+      dispatch(leavemission(id));
+    }
+  };
+
   const data = missions.map((mission) => (
-    <>
-      <div className="rows">
-        <div><h2>{mission.mission_name}</h2></div>
-        <div><p>{mission.description}</p></div>
-        <div><p>Not a member</p></div>
-        <div><button type="button">Join mission</button></div>
+    <div key={mission.mission_id} className="rows">
+      <div className="innerdiv"><h2>{mission.mission_name}</h2></div>
+      <div className="innerdiv"><p className="description">{mission.description}</p></div>
+      {mission.reserved ? <div className="container innerdiv"><p className="member">Active Member</p></div> : <div className="container innerdiv"><p className="notmember">Not a member</p></div>}
+      <div className="container innerdiv">
+        {mission.reserved ? <button type="button" className="Leave-button" onClick={() => handleclick(mission.mission_id, mission.reserved)}>Leave Mission</button>
+          : <button type="button" className="Join-button" onClick={() => handleclick(mission.mission_id, mission.reserved)}>Join Mission</button> }
       </div>
-    </>
+    </div>
   ));
 
   useEffect(() => {
@@ -26,6 +35,12 @@ const Missions = () => {
   return (
     <>
       <div className="wrapmissions">
+        <div className="rows">
+          <div className="innerdiv"><h2>Mission</h2></div>
+          <div className="innerdiv"><h2>Desription</h2></div>
+          <div className="innerdiv"><h2>Status</h2></div>
+          <div className="innerdiv" />
+        </div>
         {data}
       </div>
     </>
