a32a0195791e9cdd42b9c8130fce84e6c66220cc
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-07-25 04:46:59 +0200

    Rocket page completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketSlice from './slices/rocketSlice/rocketSlice';
 
 const store = configureStore({
   reducer: {
+    rocket: rocketSlice,
   },
 });
 
