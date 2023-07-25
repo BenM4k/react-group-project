52780f5866d6f6e28c9817e5a18333dd0629afa0
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-07-25 10:53:16 +0200

    Update Profile.jsx

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -5,8 +5,11 @@ const Profile = () => {
   const { rockets } = useSelector((store) => store.rocket);
   return (
     <div>
-      <div className="joined-missions" />
+      <div className="joined-missions">
+        <h2>My missions</h2>
+      </div>
       <div className="reserved-rockets">
+        <h2>My Rockets</h2>
         <ul>
           {rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
             <li key={rocket.id}>{rocket.name}</li>
