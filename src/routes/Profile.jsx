3d9f82039e7b3db65606092be3842d33037ca994
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-07-25 10:48:54 +0200

    Added rockets to the profile

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+const Profile = () => {
+  const { rockets } = useSelector((store) => store.rocket);
+  return (
+    <div>
+      <div className="joined-missions" />
+      <div className="reserved-rockets">
+        <ul>
+          {rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
